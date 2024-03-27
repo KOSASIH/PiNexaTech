b4c95025726b4024bb13b98e64e25e4b3155d37a
KOSASIH <kosasihg88@gmail.com>
2024-03-27 22:56:45 +0700

    Create Button.js

---
+++ src/frontend/components/Button/Button.js
@@ -0,0 +1,14 @@
+// frontend/components/Button/Button.js
+
+import React from 'react';
+import './Button.css';
+
+const Button = ({ onClick, children }) => {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
