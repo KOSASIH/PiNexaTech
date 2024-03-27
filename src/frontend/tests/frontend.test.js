9bbfea72359718c31307f4eb759c9fe5adfe5787
KOSASIH <kosasihg88@gmail.com>
2024-03-27 23:34:48 +0700

    Delete src/frontend/tests directory

--- src/frontend/tests/frontend.test.js
+++
@@ -1,9 +0,0 @@
-// tests/frontend.test.js
-
-// Example test suite for frontend components
-import { formatDate } from '../shared/utils';
-
-test('formats date correctly', () => {
-  const date = new Date('2022-01-01T00:00:00');
-  expect(formatDate(date)).toBe('January 1, 2022');
-});
